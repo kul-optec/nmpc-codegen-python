2b2382a22ae67866d9bc3a9878239b5835f17291
Willem Melis <willem.melis@student.kuleuven.be>
2018-12-08 13:29:40 +0100

    first test.c is generated

--- src/nmpccodegen/Cfunctions/IndicatorBoxFunction_new.py
+++ src/nmpccodegen/Cfunctions/IndicatorBoxFunction_new.py
@@ -1,4 +1,4 @@
-#from .source_file_operations import Source_file_generator
+# from .source_file_operations import Source_file_generator
 from source_file_operations import Source_file_generator
 
 
@@ -13,8 +13,11 @@ class IndicatorBoxFunction_new:
         source_file.open()
 
         """ what is the dimension? is this 2?? check this"""
-        struct_values = ["2", self._lower_limit, self._upper_limit,"1e6"]
-        source_file.write_instantiate_struct("data","indicator_box", struct_values, indent=1)
+        struct_values = ["2", str(self._lower_limit), str(self._upper_limit), "1e6"]
+        source_file.write_instantiate_struct("data", "indicator_box", struct_values, indent=1)
+
+        init_values = ["2", str(self._lower_limit), str(self._upper_limit)]
+        source_file.write_function_call("init_indicator_box", init_values, indent=1)
 
         source_file.write_line("return prox_indicator_box(&data,input,gamma);", indent=1)
         source_file.close()
@@ -26,7 +29,7 @@ class IndicatorBoxFunction_new:
 
 
 def main():
-    test = IndicatorBoxFunction_new([1, 2], [3, 4])
+    test = IndicatorBoxFunction_new(1, 2)
     test.generate_c_code("test.c")
 
 
